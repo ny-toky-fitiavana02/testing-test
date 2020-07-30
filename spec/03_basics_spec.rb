1786ead88dfc53d572dfbbbe94d5b6e5069cc50b
ny-toky-fitiavana02 <68009100+ny-toky-fitiavana02@users.noreply.github.com>
2020-07-30 16:28:20 +0300

    Update 03_basics_spec.rb

--- spec/03_basics_spec.rb
+++ spec/03_basics_spec.rb
@@ -3,7 +3,7 @@
 require_relative '../lib/03_basics'
 
 describe 'who is the biggest number' do
-  
+    # Hint: 'def who_is_bigger(a, b, c)'
   it 'tells me the biggest' do
     expect(who_is_bigger(84, 42, nil)).to eq("nil detected") 
     expect(who_is_bigger(nil, 42, 21)).to eq("nil detected")
@@ -14,8 +14,8 @@ describe 'who is the biggest number' do
 end
 
 describe 'crazy stuff on strings' do
-  
-  
+    # Reverse, upcase then removes all L, T and A.
+    # Hint: google ruby string
   it 'does crazy stuff on strings' do
     expect(reverse_upcase_noLTA("Tries this at Home, Kids"))
       .to eq("SDIK ,EMOH  SIH SEIR")
@@ -28,7 +28,11 @@ end
 
 
 describe '42 finder' do
- 
+  # array_42 takes an array as parameter and returns:
+  # - true if there's a 42 in the array items
+  # - false otherwise
+  # Hint: Should be 2 lines (and can be one :)
+  # Hint: google ruby array each
   it 'finds 42' do
     expect(array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 10])).to eq false
     expect(array_42([1, 2, 3, 4, 5, 6, 7 , 8, 9, 42, 21, 10.5])).to eq true
