180ca9b4cbd2e1828cf8f0451469d8718dc7e15b
ny-toky-fitiavana02 <68009100+ny-toky-fitiavana02@users.noreply.github.com>
2020-07-30 16:24:51 +0300

    Update 03_basics_spec.rb

--- spec/03_basics_spec.rb
+++ spec/03_basics_spec.rb
@@ -15,6 +15,7 @@ end
 
 describe 'crazy stuff on strings' do
   
+  
   it 'does crazy stuff on strings' do
     expect(reverse_upcase_noLTA("Tries this at Home, Kids"))
       .to eq("SDIK ,EMOH  SIH SEIR")
@@ -36,6 +37,15 @@ end
 
 
 describe 'crazy stuff on arrays' do
+  # The magic_array function takes an array of number or an array of
+  # array of number as parameter and return the same array :
+  # - flattened (i.e. no more arrays in array)
+  # - sorted
+  # - with each number multiplicated by 2
+  # - with each multiple of 3 removed
+  # - with each number duplicate removed (any number should appear only once)
+  # - sorted
+  # BONUS : You can do this in one line less than 55 chars
  
   it 'does crazy stuff on Arrays' do
     expect(magic_array([1, 2, 3, 4, 5, 6]))
